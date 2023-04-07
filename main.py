f7b3ca3efefe921741e3aedf6a80de9472a02a72
satya bhusan prusty <satyabhusan9853@gmail.com>
2023-04-07 14:01:06 +0530

    Created a ML Model Using Support Vector Classifier and compared different kenel functions

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+from IPython.display import clear_output
+import time
+import sys
